c5fb3ba3cb3c4456109e91bc1c5b29a97e50a4c7
Amir <amir.mn.mosio@gmail.com>
2026-01-11 19:43:30 +0100

    🔥Remove feedback dialog

--- lib/application.dart
+++ lib/application.dart
@@ -6,8 +6,6 @@ import 'package:who_mobile_project/app_core/theme/colors.dart';
 import 'package:who_mobile_project/app_core/theme/theme.dart';
 import 'package:who_mobile_project/generated/i18n/app_localizations.dart';
 import 'package:who_mobile_project/routing_config/base_navigator_route_builder.dart';
-import 'package:who_mobile_project/ui/feedback/feedback_floating_button.dart';
-import 'package:who_mobile_project/app_core/config/environment_constants.dart';
 import 'package:who_mobile_project/general/constants/available_languages.dart';
 
 class MyApp extends ConsumerStatefulWidget {
@@ -23,10 +21,6 @@ class MyApp extends ConsumerStatefulWidget {
 
 class _MyAppState extends ConsumerState<MyApp> {
   final GoRouter baseNavRouter = baseNavRouterBuilder();
-
-  // GlobalKey for screenshot capture (used in builder below)
-  // ignore: unused_field
-  final GlobalKey _appScreenKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -53,24 +47,6 @@ class _MyAppState extends ConsumerState<MyApp> {
           ],
           locale: const Locale('it', 'IT'), // Default: Italian
           supportedLocales: AvailableLanguage.allLocales,
-          builder: (context, child) {
-            return Stack(
-              children: [
-                // Wrap entire app navigation with RepaintBoundary for screenshots
-                RepaintBoundary(
-                  key: _appScreenKey,
-                  child: child ?? const SizedBox.shrink(),
-                ),
-                // Global feedback button - appears on all pages in staging
-                if (Constants.env == Environment.staging ||
-                    Constants.env == Environment.local)
-                  FeedbackFloatingButton(
-                    screenKey: _appScreenKey, // Pass key for screenshot capture
-                    bottom: 100, // Position from bottom
-                  ),
-              ],
-            );
-          },
         ),
       ),
     );
